a59e962d43538c970a261b0a19148ebfc6ecce2e
Moameneldriney <54772371+Moameneldriney@users.noreply.github.com>
2019-12-20 14:50:40 +0200

    detaiels

--- js/main.js
+++ js/main.js
@@ -354,7 +354,16 @@ $(document).ready(function(){
         });      
 
 
-
+        function slideUp(el) {
+          var elem = document.getElementById(el);
+          elem.style.transition = "all 2s ease-in-out";
+          elem.style.height = "0px";
+        }
+        function slideDown(el) {
+          var elem = document.getElementById(el);
+          elem.style.transition = "all 2s ease-in-out";
+          elem.style.height = "400px";
+        }
 
 
 
